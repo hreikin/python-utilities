8fb4a22b7f3fa8623432355751b0af6b8f68c3cc
Michael Haslam <hreikin@gmail.com>
2022-01-12 16:01:48 +0000

    Remove unwanted code and references to self.

--- simple_menu.py
+++ simple_menu.py
@@ -1,4 +1,4 @@
-def ask_question(self, question="Do you want to do this action again (yes/no) ? "):
+def ask_question(question="Do you want to do this action again (yes/no) ? "):
     CONFIRM = "Are you sure you want to do this (yes/no) ? "
     ask = question or CONFIRM
     val = input(ask).lower()
@@ -8,14 +8,14 @@ def ask_question(self, question="Do you want to do this action again (yes/no) ? 
         print("OK, cancelling.")
         return False
     # Invalid input response.
-    self.invalid_input()
-    return self.ask_question(question)
+    invalid_input()
+    return ask_question(question)
 
-def retry_question(self):
-    ask_to_retry = self.ask_question()
+def retry_question():
+    ask_to_retry = ask_question()
     return ask_to_retry
 
-def print_options(self, options):
+def print_options(options):
     """Prints something like this for a dict of options:
 
     e.g. from complex options:
@@ -38,15 +38,15 @@ def print_options(self, options):
         row = f"{index+1}. {sub_dict['title']}"
         print(row)
 
-def main_loop(self, options):
+def main_menu(options):
     running = True
     while running:
 
         print("\nWelcome to the address book app.")
-        self.print_options(options)
+        print_options(options)
 
         q = "Type in a number to make a selection: "
-        int_current_selection = self.ask_number(q)
+        int_current_selection = ask_number(q)
 
         choices = tuple(options.values())
         # Takes the users input and runs the appropriate function. If the input is invalid it asks
@@ -56,7 +56,7 @@ def main_loop(self, options):
         except IndexError:
             selection = {
                 'title': "Option does not exist.",
-                'func': self.invalid_input,
+                'func': invalid_input,
             }
 
         print(selection['title'])
@@ -66,67 +66,21 @@ def main_loop(self, options):
         if running is None:
             running = True
 
-def ask_number(self, q=None):
+def ask_number(q=None):
     default = "Type in a number to make a selection: "
     try:
         val = input(q or default)
         int_val = int(val)
     except ValueError as e:
-        self.invalid_input()
-        return self.ask_number(q)
+        invalid_input()
+        return ask_number(q)
 
     return int_val
 
-def invalid_input(self):
+def invalid_input():
     default = "Invalid input, please try again."
     print(default)
 
-def close_statement(self):
+def close_statement():
     print("Ok, exiting program now.")
     return False
-
-def main():
-	"""Instantiates/runs the AddressBookApp and defines a menu via a dictionary.
-	
-	Instantiates/runs the AddressBookApp and defines a menu via a dictionary 
-	with the relevant functions.
-	"""
-	app = AddressBookApp()
-	options = {
-		'view': {
-			'title':"View all contacts.",
-			'func': app.view_all
-		},
-		'search': {
-			'title':"Search all contacts.",
-			'func': app.search_contact
-		},
-		'add': {
-			'title':"Add a new contact.",
-			'func': app.add_contact
-		},
-
-		'edit': {
-			'title':"Edit an existing contact.",
-			'func': app.edit_contact
-		},
-
-		'delete': {
-			'title':"Delete a contact.",
-			'func': app.delete_contact
-		},
-
-		'exit': {
-			'title':"Exit the program.",
-			'func': app.close_statement
-		},
-
-	}
-	# global options
-
-	run = app.main_loop(options)
-	print("Goodbye!")
-	return run
-
-if __name__ == '__main__':
-	main()
